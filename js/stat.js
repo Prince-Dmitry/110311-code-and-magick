76d2bff04af89d5c78ab36eca8a743d664827be9
Dmitry <bens1iller@gmail.com>
2018-08-16 17:25:37 +0300

    Сделано

--- js/stat.js
+++ js/stat.js
@@ -1,62 +1,53 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(105, 15, 425, 275);
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'; // white;
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+  
+  ctx.fillStyle = '#000'; // black;
+  ctx.font = '16px PT Mono';
 
-  var getWindow = function (attributeX, attributeY, windowWidth, windowHeight, shadowOfWindow) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.strokeRect(attributeX, attributeY, windowWidth, windowHeight);
-    ctx.fillRect(attributeX + shadowOfWindow, attributeY + shadowOfWindow, windowWidth, windowHeight);
-    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-    ctx.fillRect(attributeX, attributeY, windowWidth, windowHeight);
-  };
-
-  var getText = function (attributeX, attributeY, textColor, textSize, textFont) {
-    ctx.fillStyle = 'textColor';
-    ctx.font = textSize + 'px  ' + textFont;
-    ctx.fillText('Ура вы победили!', attributeX, attributeY);
-    ctx.fillText('Список результатов:', attributeX, attributeY + 20);
-  };
-
-  getWindow(100, 10, 420, 270, 10);
-  getText(100, 10, '#000', 16, 'PT Mono');
-
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+  
   var max = -1;
-  var getMaximum = function (ourMassive) {
-    for (var i = 0; i < ourMassive.length; i++) {
-      var variable = ourMassive[i];
-      if (variable > max) {
-        max = variable;
-      }
+  var maxIndex = -1;
+
+  for (var i = 0 ; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+      maxIndex = i;
     }
-    return max;
-  };
+  }
+  
+  var histogramWidth = 150;              // px;
+  var step = histogramWidth / (max - 0); // px;
+  
+  var barWidth = 40;       // px; 
+  var indent = 90;          // px;
+  var initialX = 140;       // px;
+  var initialY = 240;        // px;
+  var lineHeight = 15;      // px;
+  var histogramMargin = 20; // px;
 
-  var getRandom = function (minO, maxO) {
+  function getRandom(minO, maxO) {
     return (Math.random() * (maxO - minO) + minO);
-  };
-
-  var getRandomColorOpacity = function (red, green, blue) {
-    var getRandomOpacity = getRandom(1, 0.1);
-    ctx.fillStyle = rgba(red, green, blue,' + getRandomOpacity + ');
-    return ctx.fillStyle;
-  };
-
-  var maxOfTimes = getMaximum(times);
-
-  var histogramWidth = 150;
-  var step = histogramWidth / (maxOfTimes - 0);
+  }
 
   for (var i = 0; i < times.length; i++) {
+    var getRandomOpacity = getRandom(0.2, 0.9);
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomOpacity + ')';
+    ctx.fillRect(initialX + (indent * i), initialY - times[i] * step, barWidth, times[i] * step);
     ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
-    ctx.fillText(names[i], 120 + 90 * i, 260);
-    ctx.fillText(Math.round(times[i]), 120 + 90 * i, 230 - times[i] * step);
+    ctx.fillText(names[i], initialX + (indent * i), initialY + 20);
 
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(120 + 90 * i, 240 - times[i] * step, 40, times[i] * step);
-    } else {
-      getRandomColorOpacity(0, 0, 255);
-      ctx.fillRect(120 + 90 * i, 240 - times[i] * step, 40, times[i] * step);
+      ctx.fillRect(initialX + (indent * i), initialY - times[i] * step, barWidth, times[i] * step);
     }
   }
 };
